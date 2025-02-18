412433342ad8752f5cd0aeb25f8598252110ce8a
Kabir Ghai <74025064+CypherGuy@users.noreply.github.com>
2025-02-18 15:40:33 +0000

    add DrawInformation class for sorting algorithm visualization

--- main.py
+++ main.py
@@ -1,4 +1,36 @@
+import math
 import pygame
 import random
 
 pygame.init()
+
+
+class DrawInformation:
+    BLACK = 0, 0, 0
+    WHITE = 255, 255, 255
+    GREEN = 0, 255, 0
+    RED = 255, 0, 0
+    GREY = 128, 128, 128
+    BACKGROUND_COLOR = WHITE
+
+    TOP_PAD = 150
+    SIDE_PAD = 100
+
+    def __init__(self, width, height, lst):  # lst is the starting list we want to sort
+        self.width = width
+        self.height = height
+
+        self.window = pygame.display.set_mode((self.width, self.height))
+        pygame.display.set_caption("Sorting Algorithm Visualizer")
+        self.set_list(lst)
+
+    def set_list(self, lst):
+        self.lst = lst
+        self.min_val = min(lst)
+        self.max_val = max(lst)
+
+        self.block_width = round((self.width - self.SIDE_PAD) / len(lst))
+        # This basically works out how tall each block should be in proportion to other blocks
+        self.block_height = round(
+            (self.height - self.SIDE_PAD) / (self.max_val - self.min_val))
+        self.start_x = self.SIDE_PAD // 2
